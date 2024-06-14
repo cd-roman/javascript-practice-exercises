362b2731c5b079b2afc70e5a13750e25e116a463
Roman <60399380+cd-roman@users.noreply.github.com>
2024-06-14 19:48:01 +0200

    Add examples for function declarations, expressions, and arrow functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -10,7 +10,7 @@ if (hasDriversLicense) console.log("I can drive!");
 ////    FUNCTIONS    ////
 
 function logger() {
-  console.log("My name is Jonas");
+  console.log("My name is Joey");
 }
 
 // calling / running / invoking function - these terms can be used interchangeably
@@ -28,7 +28,17 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
-const num = Number("23");
+// Functions exercise
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+const descFinland = describeCountry("Finland", 6, "Helsinki");
+const descCzechia = describeCountry("Czechia", 10, "Prague");
+const descUkraine = describeCountry("Ukraine", 36, "Kyiv");
+
+console.log(descFinland + "\n" + descCzechia + "\n" + descUkraine);
 
 ////    FUNCTION DECLARATIONS VS EXPRESSIONS    ////
 
@@ -51,6 +61,25 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 console.log(age1, age2);
 
+// Exercises
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percChina = percentageOfWorld1(1441);
+console.log(percChina);
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const percUkraine = percentageOfWorld2(36);
+console.log(percUkraine);
+
+const percUSA = percentageOfWorld2(332);
+console.log(percUSA);
+
 ////    ARROW FUNCTIONS    ////
 
 const calcAge3 = (birthYear) => 2037 - birthYear;
@@ -65,3 +94,61 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, "Joey"));
 console.log(yearsUntilRetirement(1987, "Mike"));
+
+// Exercise
+
+const percentageofWorld3 = (population) => (population / 7900) * 100;
+
+const percPortugal = percentageofWorld3(10);
+console.log(percPortugal);
+
+////    FUNCTIONS CALLING OTHER FUNCTIONS    ////
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessorAdvanced(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  console.log(apples, oranges);
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessorAdvanced(2, 3));
+
+// Exercise
+
+const describePopulation = (country, population) => {
+  const populationPercentage = percentageOfWorld1(population);
+  const description = `${country} has ${population} million people, which is about ${populationPercentage}% of the world.`;
+  console.log(description);
+};
+
+describePopulation("Ukraine", 36);
+describePopulation("Czechia", 10);
+describePopulation("USA", 332);
+
+////    REVIEWING FUNCTIONS    ////
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement2 = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1; // standard way to return a no meaning value
+  }
+};
+
+console.log(yearsUntilRetirement2(1991, "Joey"));
+console.log(yearsUntilRetirement2(1950, "Mike"));
