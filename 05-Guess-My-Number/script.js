5754cc944915013d3f9a5d935797204db0d22c10
Roman <60399380+cd-roman@users.noreply.github.com>
2024-06-19 17:59:07 +0200

    Add basic examples of the DOM manipulation

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+/*
+// Selecting the element with the class name 'message' and getting the text content of it
+console.log(document.querySelector('.message').textContent);
+
+// Changing the text content of the element with the class name 'message'
+document.querySelector('.message').textContent = 'Correct Number!';
+
+// Changing the text content of the element with the class name 'number'
+document.querySelector(".number").textContent = 12;
+
+// Changing the text content of the element with the class name 'score'
+document.querySelector(".score").textContent = 18;
+
+// Changing the value of the input element with the class name 'guess'
+document.querySelector(".guess").value = 23;
+*/
