fad3c810dff2609d90c3fc3453b239f08ea4bbf9
Roman <60399380+cd-roman@users.noreply.github.com>
2024-06-27 18:13:47 +0200

    Add enhanced object literals examples

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -4,6 +4,25 @@
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const openHours = {
+  [days[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [days[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [days[3 + 2]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+console.log(openHours);
+
 // Data needed for first part of the section
 const restaurant = {
   name: "Classico Italiano",
@@ -27,11 +46,20 @@ const restaurant = {
     },
   },
 
-  order: function (starterIndex, mainIndex) {
+  // ES6 enhanced object literals
+  openHours,
+
+  // Old way of writing methods
+  // order: function (starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+
+  // New way of writing methods introduced in ES6
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
+  orderDelivery({
     starterIndex = 1,
     mainIndex,
     time = "20:00",
@@ -42,13 +70,13 @@ const restaurant = {
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
