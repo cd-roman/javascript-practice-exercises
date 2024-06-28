36d71a42503f0653f6e5198e87154d07cc941545
Roman <60399380+cd-roman@users.noreply.github.com>
2024-06-28 02:24:54 +0200

    Add coding challenge 2

--- 09-Data-Structures-Operators/exercises.js
+++ 09-Data-Structures-Operators/exercises.js
@@ -593,3 +593,63 @@ const entries2 = Object.entries(books[0].thirdParty.goodreads);
 console.log(entries2);
 
 ///////////////////////////////////////
+
+// Coding Challenge #2
+
+/*
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
+
+GOOD LUCK 😀
+*/
+
+// Task 1
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+
+for (const [index, player] of game.scored.entries()) {
+  console.log(`Goal ${index + 1}: ${player}`);
+}
+
+// Task 2
+// Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) {
+  average += odd;
+}
+average /= odds.length;
+console.log(`Average odd: ${average}`);
+
+// Task 3
+// Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names 😉
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+
+// Task 4
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+// Gnarby: 1,
+// Hummels: 1,
+// Lewandowski: 2
+// }
+
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
+
+///////////////////////////////////////
