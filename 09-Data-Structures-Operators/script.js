d084afc315e80e55ad2df68aa318be1e0d61e091
Roman <60399380+cd-roman@users.noreply.github.com>
2024-06-29 18:10:10 +0200

    Add maps iteration examples

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -557,3 +557,43 @@ restMap.set(document.querySelector("h1"), "Heading");
 console.log(restMap);
 
 ///////////////////////////////////////
+
+// Maps: Iteration
+
+const question = new Map([
+  ["question", "What is the best programming language?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct! 🎉"],
+  [false, "Try again!"],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openHours));
+const hoursMap = new Map(Object.entries(openHours));
+console.log(hoursMap);
+
+// Maps are iterables
+
+// Quiz app
+
+console.log(question.get("question"));
+
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt("Your answer"));
+
+// console.log(question.get(question.get("correct") === answer));
+
+// Convert map to array
+
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+///////////////////////////////////////
