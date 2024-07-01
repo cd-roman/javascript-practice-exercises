92e78f7cdebdece4b8cf274c205a59fa6966dbe4
Roman <60399380+cd-roman@users.noreply.github.com>
2024-07-01 21:10:12 +0200

    Add examples of higher-order and callback functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -73,3 +73,42 @@ checkIn(flight, joey);
 // However, in JavaScript, we don't have passing by reference, we are only passing by value
 
 ///////////////////////////////////////
+
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher-order function with callback function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+
+// Example 2 of callback function
+const high5 = function () {
+  console.log("👋");
+};
+
+// Conceptually high5 is a callback function, and addEventListener is a higher-order function
+// document.body.addEventListener("click", high5);
+
+// Example 3 of callback function
+["Joey", "Monica", "Chandler"].forEach(high5);
+
+// Using higher-order functions and callbacks is a very powerful way to write more modular and reusable code
+// It creates a new level of abstraction, which makes the code more readable and maintainable
+
+///////////////////////////////////////
