b6ac6089d1bb12b927dac7e593bcfb00f4647aea
Roman <60399380+cd-roman@users.noreply.github.com>
2024-07-18 03:46:23 +0200

    Implement revealing elements on scroll functionality

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -155,6 +155,30 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(header);
 
 ///////////////////////////////////////
+
+// Reveal sections on scroll
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+///////////////////////////////////////
 // LECTURES
 ///////////////////////////////////////
 
@@ -169,9 +193,9 @@ console.log(document.body);
 // Select elements using querySelector
 // const header = document.querySelector(".header");
 
-const allSections = document.querySelectorAll(".section");
-// Logs a NodeList with all sections
-console.log(allSections);
+// const allSections = document.querySelectorAll(".section");
+// // Logs a NodeList with all sections
+// console.log(allSections);
 
 // Select elements using getElementById
 document.getElementById("section--1");
