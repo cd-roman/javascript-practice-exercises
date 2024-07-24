3063ce318bcdc79db0e87eb65dba10a3198f466c
Roman <60399380+cd-roman@users.noreply.github.com>
2024-07-25 01:02:31 +0200

    Add inheritance between 'classes' examples

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -429,3 +429,65 @@ tesla.accelerate();
 tesla.break();
 
 ///////////////////////////////////////
+
+// Inheritance Between "Classes": ES6 Classes
+
+class PersonCl2 {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log("Hey there");
+  }
+}
+
+class StudentCl extends PersonCl2 {
+  constructor(fullName, birthYear, course) {
+    // First, we call the constructor function of the parent class using the super keyword
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${2037 - this.birthYear} years old, but as a student I feel like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl("Martha Jones", 2012, "Computer Science");
+martha.introduce();
+martha.calcAge();
+
+///////////////////////////////////////
