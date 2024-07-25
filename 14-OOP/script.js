81c3c485417bfc64e1dc894714ebbbbcb4b77e2a
Roman <60399380+cd-roman@users.noreply.github.com>
2024-07-25 18:58:05 +0200

    Add chaining methods example

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -589,3 +589,9 @@ console.log(acc1);
 console.log(acc1.getMovements());
 
 ///////////////////////////////////////
+
+// Chaining Methods
+
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(2500).withdraw(4000);
+
+console.log(acc1.getMovements());
