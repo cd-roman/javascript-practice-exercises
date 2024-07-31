15547bd6046e7b9eb472bfdcbfc1525c7ffd1f13
Roman <60399380+cd-roman@users.noreply.github.com>
2024-07-31 22:21:48 +0200

    Add chaining promises example

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -157,4 +157,36 @@ const getCountryData = function (country) {
     });
 };
 
-getCountryData("portugal");
+// getCountryData("portugal");
+
+///////////////////////////////////////
+
+// Chaining promises
+
+const getCountryDataChain = function (country) {
+  // Country 1
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+    .then((response) => {
+      // response.json() returns a promise
+      return response.json();
+    })
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(
+        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+      );
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      renderCountry(data, "neighbour");
+    });
+};
+
+getCountryDataChain("portugal");
