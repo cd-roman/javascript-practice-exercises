6df67808453004a1d2c58da0f1b3c06211ff9d7f
Roman <60399380+cd-roman@users.noreply.github.com>
2024-08-12 01:46:04 +0200

    Improve code example - Part 2

--- 17-Modern-JS-Modules-Tooling/clean.js
+++ 17-Modern-JS-Modules-Tooling/clean.js
@@ -1,4 +1,6 @@
-const budget = [
+"use strict";
+
+const budget = Object.freeze([
   { value: 250, description: "Sold old TV 📺", user: "jonas" },
   { value: -45, description: "Groceries 🥑", user: "jonas" },
   { value: 3500, description: "Monthly salary 👩‍💻", user: "jonas" },
@@ -7,45 +9,70 @@ const budget = [
   { value: -20, description: "Candy 🍭", user: "matilda" },
   { value: -125, description: "Toys 🚂", user: "matilda" },
   { value: -1800, description: "New Laptop 💻", user: "jonas" },
-];
+]);
 
-const spendingLimits = {
+// Make the object immutable
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
+});
+
+// spendingLimits.jay = 200;
+// const limit = spendingLimits[user] ? spendingLimits[user] : 0;
 
 const getLimit = (limits, user) => limits?.[user] ?? 0;
 
-const addExpense = function (value, description, user = "jonas") {
-  user = user.toLowerCase();
+// Pure function
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = "jonas"
+) {
+  const cleanUser = user.toLowerCase();
 
-  const limit = getLimit(spendingLimits, user);
-
-  if (value <= limit) {
-    budget.push({ value: -value, description, user });
-  }
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value, description, user: cleanUser }]
+    : state;
 };
 
-addExpense(10, "Pizza 🍕");
-addExpense(100, "Going to movies 🍿", "Matilda");
-addExpense(200, "Stuff", "Jay");
+const newBudget1 = addExpense(budget, spendingLimits, 10, "Pizza 🍕");
+console.log(newBudget1);
+
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  "Going to movies 🍿",
+  "Matilda"
+);
+console.log(newBudget2);
+
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, "Stuff", "Jay");
+
+const checkExpenses = (state, limits) =>
+  state.map((entry) =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: "limit" }
+      : entry
+  );
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
+
+// This function is impure because it logs to the console, creating a side effect
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter((entry) => entry.value <= -bigLimit)
+    .map((entry) => entry.description.slice(-2))
+    .join(" / ");
+
+  console.log(bigExpenses);
+};
+
+// Log original budget
 console.log(budget);
 
-const checkExpenses = function () {
-  for (const entry of budget)
-    if (entry.value < -getLimit(entry.user)) entry.flag = "limit";
-};
-
-checkExpenses();
-
-const bigExpenses = function (limit) {
-  let output = "";
-  for (const entry of budget)
-    output += entry.value <= -limit ? `${entry.description.slice(-2)} / ` : ""; // Emojis are count as 2 characters
-
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-
-console.log(budget);
-bigExpenses(1000);
+// Log big expenses
+logBigExpenses(finalBudget, 500);
